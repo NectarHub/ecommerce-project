823dbaac70b03849f6a2bbf0cebd0cafeeb84425
NectarHub <wutyikyaw.2002@gmail.com>
2026-08-07 20:15:57 +0630

    Update Link to NavLink to track active links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,38 @@
+import { NavLink } from 'react-router';
+import './header.css';
 
-import {Link} from 'react-router';
-import './Header.css';
+export function Header() {
+  return (
+    <div className="header">
+      <div className="left-section">
+        <NavLink to="/" className="header-link">
+          <img className="logo"
+            src="images/logo-white.png" />
+          <img className="mobile-logo"
+            src="images/mobile-logo-white.png" />
+        </NavLink>
+      </div>
 
-export function Header(){
-    return (
-        <>
-            <div className="header">
-                <div className="left-section">
-                    <Link to="/" className="header-link">
-                        <img className="logo"
-                            src="images/logo-white.png" />
-                        <img className="mobile-logo"
-                            src="images/mobile-logo-white.png" />
-                    </Link>
-                </div>
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
 
-                <div className="middle-section">
-                    <input className="search-bar" type="text" placeholder="Search" />
+        <button className="search-button">
+          <img className="search-icon" src="images/icons/search-icon.png" />
+        </button>
+      </div>
 
-                    <button className="search-button">
-                        <img className="search-icon" src="images/icons/search-icon.png" />
-                    </button>
-                </div>
+      <div className="right-section">
+        <NavLink className="orders-link header-link" to="/orders">
 
-                <div className="right-section">
-                    <Link className="orders-link header-link" to="/orders">
+          <span className="orders-text">Orders</span>
+        </NavLink>
 
-                        <span className="orders-text">Orders</span>
-                    </Link>
-
-                    <Link className="cart-link header-link" to="/checkout">
-                        <img className="cart-icon" src="images/icons/cart-icon.png" />
-                        <div className="cart-quantity">3</div>
-                        <div className="cart-text">Cart</div>
-                    </Link>
-                </div>
-            </div> F
-        </>
-    )
+        <NavLink className="cart-link header-link" to="/checkout">
+          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <div className="cart-quantity">3</div>
+          <div className="cart-text">Cart</div>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
