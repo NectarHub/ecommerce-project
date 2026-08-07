429db1b29eb4d34da09807bb3c256c0d58b5b6f4
NectarHub <wutyikyaw.2002@gmail.com>
2026-08-07 20:50:14 +0630

    import image and render in src attribute

--- src/pages/Checkout/CheckoutHeader.jsx
+++ src/pages/Checkout/CheckoutHeader.jsx
@@ -1,4 +1,8 @@
-import { NavLink } from 'react-router'
+
+import { NavLink } from 'react-router';
+import CheckoutLockIcon from '../../assets/images/icons/checkout-lock-icon.png';
+import Logo from '../../assets/images/logo.png';
+import MobileLogo from '../../assets/images/mobile-logo.png';
 import './checkout-header.css'
 
 export function CheckoutHeader() {
@@ -8,8 +12,8 @@ export function CheckoutHeader() {
                 <div className="header-content">
                     <div className="checkout-header-left-section">
                         <NavLink to="/">
-                            <img className="logo" src="images/logo.png" />
-                            <img className="mobile-logo" src="images/mobile-logo.png" />
+                            <img className="logo" src={Logo} />
+                            <img className="mobile-logo" src={MobileLogo} />
                         </NavLink>
                     </div>
 
@@ -19,7 +23,7 @@ export function CheckoutHeader() {
                     </div>
 
                     <div className="checkout-header-right-section">
-                        <img src="images/icons/checkout-lock-icon.png" />
+                        <img src={CheckoutLockIcon} />
                     </div>
                 </div>
             </div>
