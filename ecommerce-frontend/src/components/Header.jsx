68606d9b8f233b62335b0f80a83787b855b4e9f7
NectarHub <wutyikyaw.2002@gmail.com>
2026-08-12 00:47:02 +0630

    Add search feature

--- ecommerce-frontend/src/components/Header.jsx
+++ ecommerce-frontend/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { Link, useNavigate, useSearchParams } from 'react-router';
 import CartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import Logo from '../assets/images/logo-white.png';
@@ -6,6 +8,21 @@ import { NavLink } from 'react-router';
 import './header.css';
 
 export function Header({cart}) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const searchText = searchParams.get('search');
+
+  const [search, setSearch] = useState(searchText || '');
+
+  const updateSearchInput = (e) =>{
+    setSearch(e.target.value)
+  }
+
+  const searchProducts = ()=>{
+    navigate(`/?search=${search}`);
+  }
+
   let totalQuantity = 0;
 
   cart.forEach((cartItem)=>{
@@ -23,9 +40,17 @@ export function Header({cart}) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input 
+        className="search-bar"
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={updateSearchInput}
+        />
 
-        <button className="search-button">
+        <button className="search-button"
+        onClick={searchProducts}
+        >
           <img className="search-icon" src={SearchIcon} />
         </button>
       </div>
--- ecommerce-frontend/src/pages/home/HomePage.jsx
+++ ecommerce-frontend/src/pages/home/HomePage.jsx
@@ -1,31 +1,34 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
 import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
 
-export function HomePage({ cart , loadCart}) {
-    const [products, setProducts] = useState([]);
+export function HomePage({ cart, loadCart }) {
+  const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
 
-    useEffect(() => {
-        const getHomeData = async () => {
-            const response = await axios.get('/api/products');
-            setProducts(response.data);
-        };
-        getHomeData();
+  useEffect(() => {
+    const getHomeData = async () => {
+      const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
+      setProducts(response.data);
+    };
 
-    }, []);
+    getHomeData();
+  }, [search]);
 
-    return (
-        <>
-            <title>Ecommerce Project</title>
-            <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+  return (
+    <>
+      <title>Ecommerce Project</title>
 
-            <Header cart={cart}/>
+      <Header cart={cart} />
 
-            <div className="home-page">
-                <ProductsGrid products={products}  loadCart={loadCart}/>
-            </div>
-        </>
-    );
+      <div className="home-page">
+        <ProductsGrid products={products} loadCart={loadCart} />
+      </div>
+    </>
+  );
 }
