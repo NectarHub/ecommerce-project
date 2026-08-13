cf1b2c7720c4a70e1b9bdb20d97e44766616bd5f
NectarHub <wutyikyaw.2002@gmail.com>
2026-08-13 19:49:26 +0630

    Create testing for header component

--- ecommerce-frontend/src/components/Header.jsx
+++ ecommerce-frontend/src/components/Header.jsx
@@ -33,8 +33,10 @@ export function Header({cart}) {
       <div className="left-section">
         <NavLink to="/" className="header-link">
           <img className="logo"
+            data-testid = "header-logo"
             src={Logo} />
           <img className="mobile-logo"
+            data-testid = "header-mobile-logo"
             src={MobileLogo} />
         </NavLink>
       </div>
@@ -46,22 +48,28 @@ export function Header({cart}) {
         placeholder="Search"
         value={search}
         onChange={updateSearchInput}
+        data-testid="header-search-bar"
         />
 
         <button className="search-button"
         onClick={searchProducts}
+        data-testid="header-search-button"
         >
           <img className="search-icon" src={SearchIcon} />
         </button>
       </div>
 
       <div className="right-section">
-        <NavLink className="orders-link header-link" to="/orders">
+        <NavLink className="orders-link header-link" to="/orders"
+        data-testid="header-orders-link"
+        >
 
           <span className="orders-text">Orders</span>
         </NavLink>
 
-        <NavLink className="cart-link header-link" to="/checkout">
+        <NavLink className="cart-link header-link" to="/checkout"
+        data-testid="header-cart-link"
+        >
           <img className="cart-icon" src={CartIcon} />
           <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
